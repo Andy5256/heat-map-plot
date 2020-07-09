e54067e0df929f360bfc8c8fd912007c56d8cb23
Andy5256 <wangjizhang1990@126.com>
2020-07-09 16:56:40 +0800

    hotpicture for shiftvector

---
+++ plot.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_subplot(221)
+ax1 =fig.add_subplot(221)
+ax2 =fig.add_subplot(222)
+ax3 =fig.add_subplot(223)
+ax4 =fig.add_subplot(224)
+plt.show()

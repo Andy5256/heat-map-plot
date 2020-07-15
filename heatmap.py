e139fea28152e87c2140ec9ec487fbe14be802fd
Andy5256 <wangjizhang1990@126.com>
2020-07-15 13:08:20 +0800

    gamma centered heatmap for shiftvector

--- heatmap.py
+++ heatmap.py
@@ -1,16 +1,23 @@
 #coding=utf-8
 import matplotlib.pyplot as plt
 import numpy as np
-points = np.arange(-2,2,0.1)
+points = np.arange(-10,10,0.05)
 xs,ys = np.meshgrid(points,points)
-# print(xs)
-z = np.loadtxt("shiftvec_333_40.dat")
-print(np.size(xs,1))
-print(np.size(z,1))
+z = np.loadtxt("shiftvec_333_200.dat")
+N = np.zeros((400,400))
+for i in range(200) :
+    for j in range(200) :
+        N[i,j] = N[i+200,j] = N[i,j+200] = N[i+200,j+200] = z[i,j]
+# print(N)
 fig = plt.figure()
 ax = fig.add_subplot(111)
-# plt.plot(z)
-ax.imshow(z,cmap=plt.cm.seismic)
+ax.imshow(N,cmap=plt.cm.seismic,vmin=-10,vmax=10)
+# # ax = fig.add_subplot(222)
+# # ax.imshow(z,cmap=plt.cm.seismic,vmin=-10,vmax=10)
+# # ax = fig.add_subplot(223)
+# # ax.imshow(z,cmap=plt.cm.seismic,vmin=-10,vmax=10)
+# # ax = fig.add_subplot(224)
+# # ax.imshow(z,cmap=plt.cm.seismic,vmin=-10,vmax=10)
 # plt.show()
-plt.savefig("sv_333_wt_40.png")
+plt.savefig("sv_333_200_w90.png")
 plt.close()

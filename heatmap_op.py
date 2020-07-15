f7bd76f44a53dcd65ab10aa8b14c8baaa7f2dee1
Andy5256 <wangjizhang1990@126.com>
2020-07-15 15:44:46 +0800

    sv heatmap for openmx

---
+++ heatmap_op.py
@@ -0,0 +1,23 @@
+#coding=utf-8
+import matplotlib.pyplot as plt
+import numpy as np
+points = np.arange(-5,5,0.1)
+xs,ys = np.meshgrid(points,points)
+z = np.loadtxt("shiftvec_333_100_op.dat")
+nkp = 100
+N = np.zeros((2*nkp,2*nkp))
+for i in range(nkp) :
+    for j in range(nkp) :
+        N[i,j] = N[i+nkp,j] = N[i,j+nkp] = N[i+nkp,j+nkp] = z[i,j]
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.imshow(N,cmap=plt.cm.seismic,vmin=-10,vmax=10)
+# # ax = fig.add_subplot(222)
+# # ax.imshow(z,cmap=plt.cm.seismic,vmin=-10,vmax=10)
+# # ax = fig.add_subplot(223)
+# # ax.imshow(z,cmap=plt.cm.seismic,vmin=-10,vmax=10)
+# # ax = fig.add_subplot(224)
+# # ax.imshow(z,cmap=plt.cm.seismic,vmin=-10,vmax=10)
+# plt.show()
+plt.savefig("sv_333_100_op.png")
+plt.close()

eaeb57dbbe2958405430bbdae294289751ded47a
Andy5256 <wangjizhang1990@126.com>
2020-07-16 20:04:53 +0800

    plot for 100*100*1 kmesh

--- heatmap_op.py
+++ heatmap_op.py
@@ -3,21 +3,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 points = np.arange(-5,5,0.1)
 xs,ys = np.meshgrid(points,points)
-z = np.loadtxt("shiftvec_333_100_op.dat")
+# z1 = np.loadtxt("shiftvec_333_100_op_hdch.dat")
+z = np.loadtxt("shiftvec_111_100_op_4315.dat")
+# z = np.transpose(z1)+z2
 nkp = 100
 N = np.zeros((2*nkp,2*nkp))
 for i in range(nkp) :
     for j in range(nkp) :
         N[i,j] = N[i+nkp,j] = N[i,j+nkp] = N[i+nkp,j+nkp] = z[i,j]
+# N = np.transpose(N)  # nkps in order of gamma Y ; X M
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.imshow(N,cmap=plt.cm.seismic,vmin=-10,vmax=10)
+ax.imshow(N,cmap=plt.cm.seismic,vmin=-100,vmax=100)
 # # ax = fig.add_subplot(222)
 # # ax.imshow(z,cmap=plt.cm.seismic,vmin=-10,vmax=10)
 # # ax = fig.add_subplot(223)
 # # ax.imshow(z,cmap=plt.cm.seismic,vmin=-10,vmax=10)
 # # ax = fig.add_subplot(224)
 # # ax.imshow(z,cmap=plt.cm.seismic,vmin=-10,vmax=10)
-# plt.show()
-plt.savefig("sv_333_100_op.png")
-plt.close()
+plt.show()
+# plt.savefig("sv_333_100_op_hdch.png")
+# plt.close()
--- heatmap_w90.py
+++ heatmap_w90.py
@@ -1,23 +1,24 @@
 #coding=utf-8
 import matplotlib.pyplot as plt
 import numpy as np
-points = np.arange(-10,10,0.05)
+points = np.arange(-5,5,0.1)
 xs,ys = np.meshgrid(points,points)
-z = np.loadtxt("shiftvec_333_200.dat")
-N = np.zeros((400,400))
-for i in range(200) :
-    for j in range(200) :
-        N[i,j] = N[i+200,j] = N[i,j+200] = N[i+200,j+200] = z[i,j]
-# print(N)
+z = np.loadtxt("shiftvec_111_100_w90_57.dat")
+nkp = 100
+N = np.zeros((2*nkp,2*nkp))
+for i in range(nkp) :
+    for j in range(nkp) :
+        N[i,j] = N[i+nkp,j] = N[i,j+nkp] = N[i+nkp,j+nkp] = z[i,j]
+# N = np.transpose(N)  # nkps in order of gamma Y ; X M
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.imshow(N,cmap=plt.cm.seismic,vmin=-10,vmax=10)
+ax.imshow(N,cmap=plt.cm.seismic,vmin=-100,vmax=100)
 # # ax = fig.add_subplot(222)
 # # ax.imshow(z,cmap=plt.cm.seismic,vmin=-10,vmax=10)
 # # ax = fig.add_subplot(223)
 # # ax.imshow(z,cmap=plt.cm.seismic,vmin=-10,vmax=10)
 # # ax = fig.add_subplot(224)
 # # ax.imshow(z,cmap=plt.cm.seismic,vmin=-10,vmax=10)
-# plt.show()
-plt.savefig("sv_333_200_w90.png")
-plt.close()
+plt.show()
+# plt.savefig("sv_111_200_w90.png")
+# plt.close()
